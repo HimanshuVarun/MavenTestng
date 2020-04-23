5b620e490bfa29b06bb92c24476979cf10dea69d
HimanshuVarun <himanshuvarun9756@gmail.com>
2020-04-23 20:52:04 +0530

    first commit

---
+++ src/test/java/MavenTestNg/MavenTestNg/Hello1.java
@@ -0,0 +1,16 @@
+package MavenTestNg.MavenTestNg;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+public class Hello1 
+{
+	@BeforeTest
+	public void test1()
+	{
+		System.out.println("Hello innnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn Maven testng ");
+	}
+	@Test
+	public void test2()
+	{
+		System.out.println("Hyy   nnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn Maven testng ");
+	}	
+}
